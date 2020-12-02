d7f6e62b226f6a9a4cfba74a98a0172acaf63106
jkim134 <69854275+jkim134@users.noreply.github.com>
2020-12-03 00:46:48 +0900

    changed errors on line 66 to line 79
    found out some mistakes on else if condition. changed the mistake

--- SimpleExpenseTracker(WIP).py
+++ SimpleExpenseTracker(WIP).py
@@ -63,18 +63,18 @@ def view(category, date,total):
     print('\nTotal:','$' + str(total_amount))
     newbudget = int(total)- int(total_amount)  
     print("Your new balance is $", newbudget)
-    if int(total_amount) > int(total):
-        print ("You are under budget. Please save your money or else you will go bankrupt! I am serious")
     if int(newbudget) < 500:
         print("You are under $500, please try to save more money")
-    elif int(newbudget) > 500:
-        print("Your balance is more than $", newbudget, ". You are doing great. Try to save more  :) ")
-    elif int(newbudget) > 1000:
+    elif int(newbudget) > 500 and int(newbudget) < 1000:
+        print("Your balance is more than $ 500. You are doing great. Try to save more  :) ")
+    elif int(newbudget) > 1000 and int(newbudget) < 1500:
         print("Your balance is more than $ 1000. You are doing great. Try to save more  :) ") 
-    elif int(newbudget) > 1500:
+    elif int(newbudget) > 1500 and int(newbudget) < 2000:
         print("Your balance is more than $ 1500. You are doing great. Try to save more  :) ")
-    elif int(newbudget) > 2000:
+    elif int(newbudget) > 2000 and int(newbudget) < 2500:
         print("Your balance is more than $ 2000. You are doing great. Try to save more  :) ")
+    elif int(newbudget) > 2500 :
+        print("Your balance is more than $ 2500. You are doing great. Try to save more  :) ")
     else:
         print()
 #Fuction for comparing current months spending to a month selected by the user
